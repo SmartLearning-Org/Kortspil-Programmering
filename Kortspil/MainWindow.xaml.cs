65034574a763e3cd0f0d2adc94537248fbeb2a2d
Kristian Toustrup Langborg-Hansen <ktlh@smartlearning.dk>
2021-10-28 13:42:26 +0200

    Add project files.

---
+++ Kortspil/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Kortspil
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// 
+    /// Kortene er hentet fra https://acbl.mybigcommerce.com/52-playing-cards/
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int kortnummer = Convert.ToInt32(Kort.Text);
+            string filnavn = FindBillede(kortnummer);
+            string url = $"/Billeder/{filnavn}";
+            Uri uri = new (url, UriKind.Relative);
+            BitmapImage image = new(uri);
+           
+            Billede.Source = image;
+            
+        }
+
+        private string FindBillede(int kortnummer)
+        {
+            string resultat = "2-Spar.jpg";
+
+            // Skriv din løsning her...
+
+            return resultat;
+        }
+    }
+}
